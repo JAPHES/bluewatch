c6bc5fa0582fbd10ab0ebfd37a15ada8d1147b7c
JAPHES <japhesmurithi@gmail.com>
2026-09-18 15:27:41 +0300

    Test map provider replacement

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -2,3 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 class PublicDashboardTests(TestCase):
     def test_public_impact_dashboard_is_public(self): self.assertEqual(self.client.get(reverse("dashboard:public")).status_code,200)
+    def test_public_dashboard_uses_configured_map_provider(self):
+        response=self.client.get(reverse("dashboard:public"))
+        self.assertContains(response,"services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map")
+        self.assertNotContains(response,"tile.openstreetmap.org")
